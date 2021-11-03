e9852b9ae1d3f68b4fdbd58015d50f7eefef7889
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-03 18:01:06 -0400

    connection done

--- play.js
+++ play.js
@@ -1,14 +1,20 @@
 const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const name = "tarin";
-  const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: 50541// PORT number here,
-  });
+
+
+const conn = connect();
+console.log("Connecting ...");
+// // establishes a connection with the game server
+// const connect = function () {
+//   const name = "tarin";
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',// IP address here,
+//     port: 50541// PORT number here,
+//   });
 
   // interpret incoming data as text
+  const name = "tarin";
   conn.setEncoding("utf8");
   conn.write(`${name} has connected!!!`);
 const stdin = process.stdin;
@@ -20,14 +26,6 @@ conn.on('data', (data) => {
     conn.write(`${name}: ${input}`);
   })
 
-  return conn;
-};
+  // return conn;
+//};
 
-
-
-
-
-  
-
-console.log("Connecting ...");
-connect();
